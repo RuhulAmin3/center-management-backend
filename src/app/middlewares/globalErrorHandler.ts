f95ecdf0b83e4b8b22c1202098d5318c48484bfd
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2023-07-09 15:38:35 +0600

    add global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,8 +1,13 @@
 import { ErrorRequestHandler } from "express";
+import { IGenericErrMessage } from "../../types/error";
 
 export const globalErrorHandler: ErrorRequestHandler = (
   err,
   req,
   res,
   next
-) => {};
+) => {
+  let statusCode: number = 500;
+  let message: string = "internal server error";
+  let errorMessages: IGenericErrMessage[] = [];
+};
