853f6acabcf41bd41255485673e1d3ce16d432fb
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2023-07-09 16:09:42 +0600

    some error handling done

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,5 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler } from "express";
 import { IGenericErrMessage } from "../../types/error";
+import { ZodError } from "zod";
+import ApiError from "../../errors/ApiError";
+import config from "../../config";
+import { handleValidationError } from "../../errors/validationError";
+import { handleZodError } from "../../errors/zodError";
+import { handleCastError } from "../../errors/castError";
 
 export const globalErrorHandler: ErrorRequestHandler = (
   err,
@@ -7,7 +14,39 @@ export const globalErrorHandler: ErrorRequestHandler = (
   res,
   next
 ) => {
-  let statusCode: number = 500;
-  let message: string = "internal server error";
+  let statusCode = 500;
+  let message = "internal server error";
   let errorMessages: IGenericErrMessage[] = [];
+  if (err?.name === "ValidationError") {
+    // validation Error handler
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (err?.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (err instanceof ApiError) {
+    statusCode = err.statusCode;
+    message = err.message;
+    errorMessages = err?.message ? [{ path: "", message: err?.message }] : [];
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorMessages = err?.message ? [{ path: "", message: err?.message }] : [];
+  }
+
+  // generic error format send for frontend;
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== "production" ? err?.stack : undefined,
+  });
 };
--- src/types/error.ts
+++ src/types/error.ts
@@ -2,3 +2,9 @@ export type IGenericErrMessage = {
   path: string | number;
   message: string;
 };
+
+type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrMessage[];
+};
